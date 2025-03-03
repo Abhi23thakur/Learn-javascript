143671541a6a4b73be4602984352e555bdbec2c9
Abhishek Anand <127669599+Abhi23thakur@users.noreply.github.com>
2025-03-03 08:02:38 +0000

    conversion of datatype

---
+++ javascript/03_conversion_operation.js
@@ -0,0 +1,25 @@
+// let score = 8579874;
+// let valueNumber = String(score);
+// console.log(typeof (valueNumber));
+
+
+// let score = "Abhishek";
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+
+// Note:--
+// "33" => 33
+// "33sjfhs" => NaN
+// true => 1
+// false => 0
+
+
+// let isLoggedin = "";
+// let booleanIsLoggedIn = Boolean (isLoggedin);
+// console.log(booleanIsLoggedIn);
+
+// Note:-  
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "Abhishek" => true;
